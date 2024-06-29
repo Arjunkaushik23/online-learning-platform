def4b5dd4b48eda3e7c5194347b3d114671d16b5
Arjun Kaushik <88938626+Arjunkaushik23@users.noreply.github.com>
2024-06-29 10:33:49 +0530

    Do the required changes for assignments service

--- assignment-service/src/main/java/com/example/repository/AssigmentRepository.java
+++ assignment-service/src/main/java/com/example/repository/AssigmentRepository.java
@@ -3,5 +3,11 @@ package com.example.repository;
 import com.example.entity.Assignment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AssigmentRepository extends JpaRepository<Assignment, Long> {
+    List<Assignment> findByCourseId(Long courseId);
+    List<Assignment> findByUserId(Long userId);
+
+    List<Assignment> findByProgressId(Long progressId);
 }
--- user-service/src/main/java/com/example/entity/Assignment.java
+++ user-service/src/main/java/com/example/entity/Assignment.java
@@ -1,31 +1,21 @@
 package com.example.entity;
 
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Assignment {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long userId;
     private String assignmentName;
     private Long courseId;
     private Long progressId;
     private String description;
+
 }
